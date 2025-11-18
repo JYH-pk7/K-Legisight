d8a7aa967c3c3d09b191c96a4238bac1f3ef635e
mnwldnjs-lgtm <mnwldnjs@gmail.com>
2025-11-19 00:18:20 +0900

    preprocess_model_test_all_finished

--- evaluation_model/answers/answers_Gyoyuk.py
+++ evaluation_model/answers/answers_Gyoyuk.py
@@ -1,84 +1,63 @@
+# evaluation_model/answers_gyoyuk.py
+# -----------------------------
+# answer standard: 사람이 만든 정답 세트
+
 answer_segments = {
-    "51197": [
+    50781: [
         {
             "delib_order": 1,
-            "agenda_items": [1],
-            "agenda_range_str": "의사일정 제1항"
+            "agenda_items": [1, 2, 3, 4, 5, 6],
+            "agenda_range_str": "의사일정 제1항부터 제6항까지",
         },
         {
             "delib_order": 2,
-            "agenda_items": [2,3,4],
-            "agenda_range_str": "의사일정 제2항부터 제4항까지"
+            "agenda_items": [5],
+            "agenda_range_str": "의사일정 제5항",
         },
         {
             "delib_order": 3,
-            "agenda_items": [5,6,7,8,9],
-            "agenda_range_str": "의사일정 제5항부터 제9항까지"
+            "agenda_items": [7, 8],
+            "agenda_range_str": "의사일정 제7항부터 제8항까지",
         },
         {
             "delib_order": 4,
-            "agenda_items": [2,3,4],
-            "agenda_range_str": "2항과 3항, 4항에 대해서 의견 있으신 분"
+            "agenda_items": [9],
+            "agenda_range_str": "의사일정 제9항",
         },
         {
             "delib_order": 5,
-            "agenda_items": [5,6,7,8,9],
-            "agenda_range_str": "의사일정 제5항부터 제9항까지"
+            "agenda_items": [10, 11, 12],
+            "agenda_range_str": "의사일정 제10항부터 제12항까지",
         },
         {
             "delib_order": 6,
-            "agenda_items": [10,11,12,13],
-            "agenda_range_str": "의사일정 제10항부터 제13항까지"
+            "agenda_items": [13],
+            "agenda_range_str": "의사일정 제13항",
         },
         {
             "delib_order": 7,
-            "agenda_items": [49,50],
-            "agenda_range_str": "제49항과 제50항 윤영덕 의원, 권인숙 의원이 각각 대표발의한 2건의 사립학교법 일부개정법률안까지 심사한 후에 의결하도록 하겠습니다."
+            "agenda_items": [14, 15, 16, 17, 18],
+            "agenda_range_str": "의사일정 제14항부터 제18항까지",
         },
         {
             "delib_order": 8,
-            "agenda_items": [14],
-            "agenda_range_str": "이어서 의사일정 제14항"
+            "agenda_items": [17],
+            "agenda_range_str": "의사일정 제17항",
         },
         {
             "delib_order": 9,
-            "agenda_items": [15,16],
-            "agenda_range_str": "의사일정 제15항과 제16항"
+            "agenda_items": [18],
+            "agenda_range_str": "의사일정 제18항",
         },
         {
             "delib_order": 10,
-            "agenda_items": [17,18,19,20],
-            "agenda_range_str": "의사일정 제17항부터 제20항까지"
+            "agenda_items": [19, 20, 21, 22, 23],
+            "agenda_range_str": "의사일정 제19항부터 제23항까지",
         },
         {
             "delib_order": 11,
-            "agenda_items": [45,46,47,48],
-            "agenda_range_str": "의사일정 제45항부터 제48항까지"
+            "agenda_items": [24, 25],
+            "agenda_range_str": "의사일정 제24항부터 제25항까지",
         },
-        {
-            "delib_order": 12,
-            "agenda_items": [21],
-            "agenda_range_str": "다음으로 의사일정 제21항"
-        },
-        {
-            "delib_order": 13,
-            "agenda_items": [22,23,24,25,26],
-            "agenda_range_str": "다음으로 의사일정 제22항부터 제26항까지"
-        },
-        {
-            "delib_order": 14,
-            "agenda_items": [27,28,29],
-            "agenda_range_str": "다음으로 의사일정 제27항부터 제29항까지"
-        },
-        {
-            "delib_order": 15,
-            "agenda_items": [54,55,56],
-            "agenda_range_str": "의사일정 제54항부터 제56항까지"
-        },
-        {
-            "delib_order": 16,
-            "agenda_items": [51],
-            "agenda_range_str": "의사일정 제51항"
-        }
     ]
 }
